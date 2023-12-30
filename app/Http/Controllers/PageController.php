917abea12c8b31cb637da5a7f446525c5fbd7d20
anakoisaac <anakoisaac@gmail.com>
2023-12-30 02:16:31 +0000

    Visitors counter added

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\VisitorCounter;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function comingSoon(Request $request)
     {
+        new VisitorCounter($request);
         return view('coming-soon');
     }
 }
